676ca5611262aca1327555397f67011078f35d43
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-04-22 14:05:08 -0700

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let remainingSeconds = 10;
 let interval = null;
 
+//Set timer amount in seconds on toggle switch
 function setTimer(){
     const timerSwitch = document.getElementById('timer-type');
     remainingSeconds = timerSwitch.checked ? 300 : 1500;
@@ -8,6 +9,7 @@ function setTimer(){
     document.getElementById('time').textContent = convertTime();
 }
 
+//Convert total seconds to minutes & seconds
 function convertTime(){
     const minutes = parseInt(remainingSeconds / 60);
     const seconds = addLeadingZero(remainingSeconds % 60);
@@ -15,11 +17,14 @@ function convertTime(){
     return `${minutes}:${seconds}` 
 }
 
+//Add a 0 to front of minutes if < 10
 function addLeadingZero(value){
     return value < 10 ? `0${value}` : value;
 }
 
+//Start running the timer countdown
 function startTimer() {
+    document.getElementById('start-button').onclick = null;
 
     remainingSeconds -= 1;
     document.getElementById('time').textContent = convertTime();
@@ -37,14 +42,16 @@ function startTimer() {
     }, 1000);
 }
 
+//Clear the interval and reset the timer when pressing reset button
 function resetTimer() {
     clearInterval(interval);
     
-    document.getElementById('start-button').addEventListener('onclick', startTimer)
+    document.getElementById('start-button').onclick = startTimer;
 
     setTimer();
 }
 
+//Play beep noise once timer is at 0
 function playBeep() {
     var audio = new Audio('https://www.soundjay.com/button/sounds/beep-01a.mp3');  
     audio.play();
